efdfab74e2416a82267de0ef7472350559f5d78e
Charls <delrosario.charls17@gmail.com>
2022-02-27 00:58:47 +0800

    Added a function runGame that decides the winner between two parameters playerSelection and computerSelection

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ function computerPlay(){
     return choiceArray[x];
 }
 
+let computerSelection = computerPlay();
+let playerSelection = prompt("Choose rock, paper, or scissors!");
+
+
+function runGame(playerSelection,computerSelection){   
+    if((playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') ||
+    (playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper') ||
+    (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors')){
+        console.log('It\'s a tie!');
+    }   
+
+    else if((playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') ||
+    (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper')){
+        console.log('Player won!');
+    }
+
+    else{
+        console.log('Computer won!')
+    }
+}
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+console.log(runGame(playerSelection,computerSelection));
