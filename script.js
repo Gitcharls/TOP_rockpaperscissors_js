c51daef89605cc7947ee2fe316cd4854d66c4170
Charls <delrosario.charls17@gmail.com>
2022-02-27 02:00:39 +0800

    Added the final function game() which tests the winner in 5 rounds using loop

--- script.js
+++ script.js
@@ -6,29 +6,54 @@ function computerPlay(){
     return choiceArray[x];
 }
 
-let computerSelection = computerPlay();
-let playerSelection = prompt("Choose rock, paper, or scissors!");
+// let computerSelection = computerPlay();
+let computerSelection = 'rock';
 
+let playerScore = 0;
+let computerScore = 0;
 
 function runGame(playerSelection,computerSelection){   
     if((playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') ||
     (playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper') ||
     (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors')){
         console.log('It\'s a tie!');
+        console.log(`Player score: ${playerScore}`);
+        console.log(`Computer Score ${computerScore}`);
     }   
 
     else if((playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') ||
     (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') ||
     (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper')){
-        console.log('Player won!');
+        playerScore++;
+        console.log(`Player won!... Player score: ${playerScore}`);
+        console.log(`Computer Score ${computerScore}`);
     }
 
     else{
-        console.log('Computer won!')
+        computerScore++;
+        console.log(`Computer won!... Computer score: ${computerScore}`)
+        console.log(`Player score: ${playerScore}`);
+        
     }
 }
 
-console.log(playerSelection);
-console.log(computerSelection);
 
-console.log(runGame(playerSelection,computerSelection));
+//  console.log(playerSelection);
+//  console.log(computerSelection);
+
+//  console.log(runGame(playerSelection,computerSelection));
+//  console.log(playerScore);
+//  console.log(computerScore);
+
+function game(){
+    for(let i=0; i < 5; i++){
+        console.log(`Round ${i+1}!`);
+        let playerSelection = prompt("Choose rock, paper, or scissors!");
+        console.log(`Player choice: ${playerSelection}`);
+        console.log(`Computer choice: ${computerSelection}`);
+        runGame(playerSelection,computerSelection);
+    }
+    playerScore > computerScore ? console.log('Player is the alpha!') : console.log('Computer is the alpha!');
+}
+
+console.log(game());
