1f26d153744e3345a06594620a4a0df7c20dee24
Charls <delrosario.charls17@gmail.com>
2022-02-28 16:37:28 +0800

    Improved the function logic-wise and added comments

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 
+//Created an array literal for the computer choice
 const choiceArray = ['rock', 'paper', 'scissors'];  
 
 function computerPlay(){
-    let x = Math.floor(Math.random() * 3);
-    return choiceArray[x];
+    let x = Math.floor(Math.random() * choiceArray.length); //Variable x will have a random value with a maximum of the choiceArray length
+    return choiceArray[x]; // Here, the x variable is used to return a string based on the random number
 }
 
-// let computerSelection = computerPlay();
-let computerSelection = 'rock';
+let computerSelection = computerPlay();
+
 
 let playerScore = 0;
 let computerScore = 0;
 
-function runGame(playerSelection,computerSelection){   
-    if((playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') ||
-    (playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper') ||
-    (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors')){
+function runGame(playerSelection,computerSelection){   //Tests the winner out of two using conditional statements
+    if((playerSelection === computerSelection)){
         console.log('It\'s a tie!');
         console.log(`Player score: ${playerScore}`);
         console.log(`Computer Score ${computerScore}`);
@@ -37,23 +36,15 @@ function runGame(playerSelection,computerSelection){
     }
 }
 
-
-//  console.log(playerSelection);
-//  console.log(computerSelection);
-
-//  console.log(runGame(playerSelection,computerSelection));
-//  console.log(playerScore);
-//  console.log(computerScore);
-
-function game(){
-    for(let i=0; i < 5; i++){
-        console.log(`Round ${i+1}!`);
+function game(){    //WIll iterate the game for 5 rounds and test the winner
+        console.log(`Round ${i+1}!`); 
         let playerSelection = prompt("Choose rock, paper, or scissors!");
         console.log(`Player choice: ${playerSelection}`);
         console.log(`Computer choice: ${computerSelection}`);
         runGame(playerSelection,computerSelection);
     }
-    playerScore > computerScore ? console.log('Player is the alpha!') : console.log('Computer is the alpha!');
+    playerScore > computerScore ? console.log('Player is the alpha!') : console.log('Computer is the alpha!'); // i used a ternary operator so it will show that the computer wins even if they have the same score for 5 rounds
+    for(let i=0; i < 5; i++){
 }
 
 console.log(game());
